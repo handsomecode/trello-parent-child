bdf32dd2341e283737e10aee7d780c4837f0ba91
Danil <danilvalov@gmail.com>
2015-12-24 17:13:14 +0600

    Adding additional data request error

--- vendor/chrome/errors-handler.js
+++ vendor/chrome/errors-handler.js
@@ -2,6 +2,26 @@
   'use strict';
 
   function sendError(e) {
+    var userData = {
+      username: '',
+      fullname: '',
+      email: ''
+    };
+
+    if (
+        typeof TrelloHelper !== 'undefined' &&
+        typeof TrelloHelper.data !== 'undefined' &&
+        typeof TrelloHelper.data.me !== 'undefined' &&
+        typeof TrelloHelper.data.me.data !== 'undefined' &&
+        typeof TrelloHelper.data.me.data.username !== 'undefined' &&
+        typeof TrelloHelper.data.me.data.fullName !== 'undefined' &&
+        typeof TrelloHelper.data.me.data.email !== 'undefined'
+    ) {
+      userData.username = TrelloHelper.data.me.data.username;
+      userData.fullname = TrelloHelper.data.me.data.fullName;
+      userData.email = TrelloHelper.data.me.data.email;
+    }
+
     var xhr = new XMLHttpRequest();
 
     xhr.open('POST', 'https://api.airtable.com/v0/appubz6id6ulcTeUr/Errors', true);
@@ -13,6 +33,10 @@
       fields: {
         Message: e.message,
         Date: new Date().toISOString(),
+        Version: chrome.runtime.getManifest().version,
+        Username: userData.username,
+        Fullname: userData.fullname,
+        Email: userData.email,
         Notes: e.stack,
         Browser: navigator.userAgent,
         URL: window.location.href
